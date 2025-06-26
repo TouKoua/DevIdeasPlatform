a8b3ce315219acb9a6a47dc1e482bfcb9007ff8d
Nathan Huang <nh060501@gmail.com>
2025-06-26 03:16:35 -0500

    Updated ProjectDetailPage.tsx

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link, useLocation } from 'react-router-dom';
 import { useProjects } from '../context/ProjectContext';
 import Badge from '../components/ui/Badge';
 import Button from '../components/ui/Button';
